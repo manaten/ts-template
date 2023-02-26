3a62ae4f4b5f4667f9a5a58a25e41c04d5f4f6e5
manaten <manaten@manaten.net>
2023-02-26 15:04:14 +0900

    fix(tsconfig.json): fix resolution rule.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import fs from "fs/promises";
 
+import { test } from "./lib/test";
+
 async function main() {
   const content = await fs.readFile("package.json");
   console.log(content.toString());
+  test();
 }
 
 main().catch(e => console.error(e));
