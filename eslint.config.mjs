3b240c4bd997f5f7d9d830fd70ed8636cebb26d9
manaten <manaten@manaten.net>
2025-03-16 00:16:18 +0900

    chore: update packages.

---
+++ eslint.config.mjs
@@ -0,0 +1,78 @@
+// @ts-check
+
+import js from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
+import eslintConfigPrettier from "eslint-config-prettier";
+// @ts-expect-error 型定義がないため
+import importPlugin from "eslint-plugin-import";
+import globals from "globals";
+import tsEslint from "typescript-eslint";
+
+export default tsEslint.config(
+  {
+    ignores: ["dist", "node_modules"],
+  },
+  js.configs.recommended,
+  importPlugin.flatConfigs.recommended,
+  tsEslint.configs.recommended,
+  tsEslint.configs.eslintRecommended,
+  eslintConfigPrettier,
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            ["builtin", "external"],
+            "internal",
+            ["parent", "index", "sibling", "object"],
+          ],
+
+          pathGroups: [
+            {
+              pattern: "*.scss",
+
+              patternOptions: {
+                matchBase: true,
+              },
+
+              group: "unknown",
+              position: "after",
+            },
+          ],
+
+          "newlines-between": "always",
+
+          alphabetize: {
+            order: "asc",
+          },
+        },
+      ],
+
+      "import/named": 0,
+      "import/no-unresolved": 0,
+      "no-undef": 0,
+      "no-unused-vars": 0,
+      "no-var": 2,
+      "compat/compat": 0,
+    },
+  },
+);
